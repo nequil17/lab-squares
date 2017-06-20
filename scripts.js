55d75d922be8461c7438e499083ec8eda5b67e21
InnovateBham_13 <InnovateBham_13@Admins-iMac.local>
2017-06-20 15:35:12 -0700

    new

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 var btn = document.createElement("BUTTON");        
-var t = document.createTextNode("CLICK ME");       
+var t = document.createTextNode("CLICK ME"); 
+var counter = 0      
+// var boxNum = [i > 0]
 btn.appendChild(t);                                
 document.body.appendChild(btn);
 btn.style.border='3px solid blue';
@@ -10,13 +12,26 @@ btn.style.backgroundcolor='red';
 btn.addEventListener('click', function() {
     var box = document.createElement('div');
     box.className = 'divBox';
-    box.style.width='300px';
-    box.style.height='300px';
-    box.style.backgroundColor='black';
-    box.style.marginBottom='1em'
+    box.style.width = '200px';
+    box.style.height = '200px';
+    box.style.backgroundColor = 'black';
+    box.style.marginBottom = '1em'
     document.body.appendChild(box);
-});
-btn.addEventListener('dblclick', function(){
+    
+box.addEventListener('click', function(){
+    box.style.backgroundColor = randomColor();
+    
+    function randomColor() {
+        var randomRed = Math.floor(Math.random() * 350);
+        var randomGreen = Math.floor(Math.random() * 350);
+        var randomBlue = Math.floor(Math.random() * 350);
+        var randomColor = "rgb("+randomRed+","+randomGreen+","+randomBlue+")";
+
+    return randomColor;
+}
+});    
+}); 
+btn.addEventListener('dblclick', function() {
 
 });
 });
