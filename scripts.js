6e333a883854dba0b335e4239375962ef1b9abb1
InnovateBham_13 <InnovateBham_13@Admins-iMac.local>
2017-06-21 15:50:37 -0700

    ...

--- scripts.js
+++ scripts.js
@@ -1,37 +1,44 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-var btn = document.createElement("button");        
-var t = document.createTextNode("CLICK ME"); 
-var counter = 0      
-// var boxNum = [i > 0]
-btn.appendChild(t);                                
-document.body.appendChild(btn);
-btn.style.border='3px solid blue';
-btn.style.backgroundcolor='red';
 
-btn.addEventListener('click', function() {
-    var box = document.createElement('div');
-    box.className = 'divBox';
-    box.style.width = '200px';
-    box.style.height = '200px';
-    box.style.backgroundColor = 'black';
-    box.style.marginBottom = '1em'
-    document.body.appendChild(box);
-    
-box.addEventListener('click', function(){
-    box.style.backgroundColor = randomColor();
-    
+    var btn = document.createElement("button");        
+    var t = document.createTextNode("CLICK ME"); 
+    var counter = 0;
+    btn.appendChild(t);                                
+    document.body.appendChild(btn);
+    btn.style.border='3px solid blue';
+    btn.style.backgroundColor='red';
+
+    btn.addEventListener('click', function() {
+        var box = document.createElement('div');
+        box.className = 'divBox';
+        box.id = counter++;
+        box.style.width = '200px';
+        box.style.height = '200px';
+        box.style.backgroundColor = 'black';
+        box.style.marginBottom = '1em'
+        document.body.appendChild(box);
+        
+        box.addEventListener('click', function() {
+            box.style.backgroundColor = randomColor();
+        });
+        box.addEventListener('mouseover', function() {
+            box.innerText = (box.id);
+        });    
+        box.addEventListener('mouseout', function() {
+            box.innerText = '';
+        });
+        box.addEventListener('dblclick', function() {
+
+        });
+    });
+
     function randomColor() {
         var randomRed = Math.floor(Math.random() * 250);
         var randomGreen = Math.floor(Math.random() * 250);
         var randomBlue = Math.floor(Math.random() * 250);
         var randomColor = "rgb("+randomRed+","+randomGreen+","+randomBlue+")";
 
-    return randomColor;
-}
-});    
-}); 
-btn.addEventListener('dblclick', function() {
-
-});
+        return randomColor;
+    }
 });
